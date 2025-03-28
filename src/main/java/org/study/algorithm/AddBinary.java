d08d8efb3e949ebee87b03037be044ae3df4128f
taemin-kwon93 <4146kt@gmail.com>
2025-03-28 15:53:01 +0900

    study: leetcode
    - 67 add binary

--- src/main/java/org/study/algorithm/AddBinary.java
+++ src/main/java/org/study/algorithm/AddBinary.java
@@ -6,4 +6,23 @@ public class AddBinary {
     public String addBinary(String a, String b) {
         return new BigInteger(a, 2).add(new BigInteger(b, 2)).toString(2);
     }
+
+    public String addBinaryB(String a, String b) {
+        char[] chars = new char[Math.max(a.length(), b.length()) + 1];
+        int carry = 0;
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+
+        for (int k = chars.length - 1; k >= 0; k--) {
+            int sum = carry;
+            if (i >= 0) sum += a.charAt(i--) - '0';
+            if (j >= 0) sum += b.charAt(j--) - '0';
+            chars[k] = (char) (sum % 2 + '0');
+            carry = sum / 2;
+        }
+
+        if (chars[0] == '0')
+            return new String(chars, 1, chars.length - 1);
+        return new String(chars);
+    }
 }
