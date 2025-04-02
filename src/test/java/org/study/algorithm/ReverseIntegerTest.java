a9a5337445643a7444e9c99b96f126b5b355fcd8
taemin-kwon93 <4146kt@gmail.com>
2025-04-02 17:20:14 +0900

    study: leetcode
    - 7 reverse integer

--- src/test/java/org/study/algorithm/ReverseIntegerTest.java
+++ src/test/java/org/study/algorithm/ReverseIntegerTest.java
@@ -29,4 +29,21 @@ class ReverseIntegerTest {
         assertEquals(0, result5); // Overflow
         assertEquals(0, result6);
     }
+
+    @Test
+    void reverse2() {
+        int result1 = reverseInteger.reverse2(123);
+        int result2 = reverseInteger.reverse2(-123);
+        int result3 = reverseInteger.reverse2(120);
+        int result4 = reverseInteger.reverse2(2147483647);
+        int result5 = reverseInteger.reverse2(-2147483648);
+        int result6 = reverseInteger.reverse2(0);
+
+        assertEquals(321, result1);
+        assertEquals(-321, result2);
+        assertEquals(21, result3);
+        assertEquals(0, result4); // Overflow
+        assertEquals(0, result5); // Overflow
+        assertEquals(0, result6);
+    }
 }
