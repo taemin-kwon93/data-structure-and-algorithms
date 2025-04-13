78f0b3366a797680ec72ccc53a0064edf921d79a
taemin-kwon93 <4146kt@gmail.com>
2025-04-13 16:21:11 +0900

    refactor:

--- src/main/java/org/study/parksangkhil/Combinations.java
+++ src/main/java/org/study/parksangkhil/Combinations.java
@@ -3,26 +3,22 @@ package org.study.parksangkhil;
 import java.util.*;
 
 public class Combinations {
-    public void dfs(List<List<Integer>> results, LinkedList<Integer> elements, int n, int start, int k) {
-        if (k == 0) {
-            results.add(new ArrayList<>(elements));
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(result, new ArrayList<>(), n, k, 0);
+        return result;
+    }
+
+    public void backtrack(List<List<Integer>> result, List<Integer> path, int n, int k, int node) {
+        if (path.size() == k) {
+            result.add(new ArrayList<>(path));
             return;
         }
 
-        for (int i = start; i <= n; i++) {
-            elements.add(i);
-            dfs(results, elements, n, i + 1, k - 1);
-            // 마지막 요소를 지우기 위해 elements를 LinkedList로 선언함
-            elements.removeLast();
+        for (int i = node; i < n; i++) {
+            path.add(i + 1);
+            backtrack(result, path, n, k, i + 1);
+            path.remove(path.size() - 1);
         }
-    }
-
-
-    public List<List<Integer>> combine(int n, int k) {
-        List<List<Integer>> results = new ArrayList<>();
-
-        dfs(results, new LinkedList<>(), n, 1, k);
-
-        return results;
     }
 }
