abb1e0f56ea10c1bda98cd0d8060ea78047c7601
taemin-kwon93 <4146kt@gmail.com>
2025-08-18 20:47:24 +0900

    chore: 괄호 삭제

--- src/main/java/org/study/algorithm/CoinChangeII518.java
+++ src/main/java/org/study/algorithm/CoinChangeII518.java
@@ -5,11 +5,9 @@ public class CoinChangeII518 {
         int[] dp = new int[amount + 1];
         dp[0] = 1; // 금액 0을 만드는 방법은 "아무것도 선택하지 않기" 한 가지
 
-        for (int coin : coins) {
-            for (int i = coin; i <= amount; i++) {
+        for (int coin : coins)
+            for (int i = coin; i <= amount; i++)
                 dp[i] += dp[i - coin];
-            }
-        }
 
         return dp[amount];
     }
